36c92192be31e950dd49f60d106b9d27cc1a1299
Duy Hoang <duyhoang123@hotmail.co.uk>
2023-04-05 18:15:54 +0200

    controller testing spec

---
+++ test/controllers/SubscribedControllerSpec.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import org.scalatest.GivenWhenThen
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.must.Matchers
+
+class SubscribedControllerSpec extends AnyFunSpec with Matchers with GivenWhenThen {
+
+  describe("") {
+    it("") {
+
+    }
+    it("")
+  }
+
+}
